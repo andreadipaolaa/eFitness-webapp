1aee3514cfb94057b8d713aab0c24d6f75b94955
AndreaDiPaola <andrea.dipaola@outlook.com>
2022-06-17 19:51:19 +0200

    first commit

---
+++ eFitness-webapp/src/main/java/it/uniroma3/siw/efitness/efitnesswebapp/EFitnessWebappApplication.java
@@ -0,0 +1,13 @@
+package it.uniroma3.siw.efitness.efitnesswebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EFitnessWebappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EFitnessWebappApplication.class, args);
+    }
+
+}
---
+++ eFitness-webapp/src/test/java/it/uniroma3/siw/efitness/efitnesswebapp/EFitnessWebappApplicationTests.java
@@ -0,0 +1,13 @@
+package it.uniroma3.siw.efitness.efitnesswebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EFitnessWebappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
