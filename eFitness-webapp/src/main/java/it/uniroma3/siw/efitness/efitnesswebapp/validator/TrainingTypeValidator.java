8d82c4f5f2cf9a589f31aa1cdc8f499a8cfa8d81
Simona <smndrs.sdr@gmail.com>
2022-06-20 20:52:13 +0200

    base user html+css

--- eFitness-webapp/src/main/java/it/uniroma3/siw/efitness/efitnesswebapp/validator/TrainingTypeValidator.java
+++ eFitness-webapp/src/main/java/it/uniroma3/siw/efitness/efitnesswebapp/validator/TrainingTypeValidator.java
@@ -3,9 +3,10 @@ package it.uniroma3.siw.efitness.efitnesswebapp.validator;
 import it.uniroma3.siw.efitness.efitnesswebapp.model.TrainingType;
 import it.uniroma3.siw.efitness.efitnesswebapp.service.TrainingTypeService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 import org.springframework.validation.Errors;
 import org.springframework.validation.Validator;
-
+@Component
 public class TrainingTypeValidator implements Validator {
 
     @Autowired
