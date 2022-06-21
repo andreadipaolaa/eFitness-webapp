0cd6734093d71de976cd11cc82f24671e0bb4b23
andreadipaola <andrea.dipaola@outlook.com>
2022-06-21 13:02:52 +0200

    list-trainer test

--- eFitness-webapp/src/main/java/it/uniroma3/siw/efitness/efitnesswebapp/controller/MainController.java
+++ eFitness-webapp/src/main/java/it/uniroma3/siw/efitness/efitnesswebapp/controller/MainController.java
@@ -1,6 +1,7 @@
 package it.uniroma3.siw.efitness.efitnesswebapp.controller;
 
 import it.uniroma3.siw.efitness.efitnesswebapp.service.CourseService;
+import it.uniroma3.siw.efitness.efitnesswebapp.service.PersonalTrainerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -14,15 +15,24 @@ public class MainController {
     @Autowired
     private CourseService courseService;
 
+    @Autowired
+    private PersonalTrainerService personalTrainerService;
+
     @RequestMapping(value = {"home"}, method = RequestMethod.GET)
     public String getHome(){
         return "index.html";
     }
 
-    @RequestMapping(value = {""}, method = RequestMethod.GET)
+    @RequestMapping(value = {"courses"}, method = RequestMethod.GET)
     public String getCourses(Model model){
         model.addAttribute("courses", this.courseService.getAll());
         return "user/course-list.html";
+    }
+
+    @RequestMapping(value = {"trainers"}, method = RequestMethod.GET)
+    public String getTrainers(Model model){
+        model.addAttribute("trainers", this.personalTrainerService.getAll());
+        return "user/trainer-list.html";
     }
 
 }
